436e4f37b596ccac5a9c70bdf86132cc2b11e85b
andyhall <andy@fenomas.com>
2015-02-09 23:42:13 +0900

    Adding source to gh-pages

---
+++ bundle.js
@@ -0,0 +1,714 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+var element = document
+var options = {
+  preventDefaults: false,
+  stopPropagation: false
+}
+
+var inputs = require('../')( element, options )
+window.inputs = inputs
+
+
+inputs.bind( 'move-left',  'A', '<left>' )
+inputs.bind( 'move-right', 'D', '<right>' )
+inputs.bind( 'fire',  '<mouse 1>' )
+inputs.bind( 'RMB',  '<mouse 3>' )
+
+inputs.down.on( 'move-left',  function(){showOutput('move-left pressed')} )
+inputs.down.on( 'move-right', function(){showOutput('move-right pressed')} )
+inputs.down.on( 'fire',       function(){showOutput('fire pressed')} )
+inputs.down.on( 'RMB',        function(){showOutput('RMB pressed')} )
+inputs.up.on( 'move-left',  function(){showOutput('move-left released')} )
+inputs.up.on( 'move-right', function(){showOutput('move-right released')} )
+inputs.up.on( 'fire',       function(){showOutput('fire released')} )
+inputs.up.on( 'RMB',        function(){showOutput('RMB released')} )
+
+var bindingNames = inputs.getBindings()  
+
+
+
+
+
+
+// Example page HTML stuff
+function addDiv(txt, html) {
+  var div = document.createElement('div')
+  if (html) div.innerHTML = txt
+  else div.textContent = txt
+  document.body.appendChild(div)
+  return div
+}
+addDiv('The following keys are bound:')
+addDiv(bindingNames.sort().join(', '))
+addDiv('<p />Event output:', true)
+var textarea = document.createElement('textarea')
+textarea.style.width = "300px"
+textarea.style.height = "40px"
+document.body.appendChild(textarea)
+addDiv('Or open the console and inspect "inputs".<p />', true)
+addDiv('Current values in "inputs.state" object:<p />', true)
+var stateDiv = addDiv('',true)
+function showOutput(txt) {
+  textarea.value = txt
+}
+var ct = 0
+function showState() {
+  var s = inputs.state
+  var a = []
+  for (var name in s) a.push(name)
+  var str = ''
+  for (var i=0; i<a.length; ++i) {
+    str += a[i] + ': ' + s[a[i]] + '<br>'
+  }
+  stateDiv.innerHTML = str
+  inputs.tick()
+}
+setInterval( showState, 200 )
+},{"../":2}],2:[function(require,module,exports){
+'use strict';
+
+var vkey = require('vkey')
+var EventEmitter = require('events').EventEmitter;
+
+
+module.exports = function(domElement, options) {
+  return new Inputs(domElement, options)
+}
+
+
+/*
+ *   Simple inputs manager to abstract key/mouse inputs.
+ *        Inspired by (and where applicable stealing code from) 
+ *        game-shell: https://github.com/mikolalysenko/game-shell
+ *  
+ *  inputs.bind( 'move-right', 'D', '<right>' )
+ *  inputs.bind( 'move-left',  'A' )
+ *  inputs.unbind( 'move-left' )
+ *  
+ *  inputs.down.on( 'move-right',  function( binding, event ) {})
+ *  inputs.up.on(   'move-right',  function( binding, event ) {})
+ *
+ *  inputs.state['move-right']  // true when corresponding keys are down
+ *  inputs.state.dx             // mouse x movement since tick() was last called
+ *  inputs.getBindings()        // [ 'move-right', 'move-left', ... ]
+*/
+
+
+function Inputs(element, opts) {
+
+  // settings
+  this.element = element || document
+  opts = opts || {}
+  this.preventDefaults = !!opts.preventDefaults
+  this.stopPropagation = !!opts.stopPropagation
+
+  // emitters
+  this.down = new EventEmitter()
+  this.up = new EventEmitter()
+
+  // state object to be queried
+  this.state = {
+    dx: 0, dy: 0
+  }
+
+  // internal state
+  this._bindings = {}
+  this._keyStates = {}
+  this._bindPressCounts = {}
+
+  // register for dom events
+  this.initEvents()
+}
+
+
+/*
+ *
+ *   PUBLIC API 
+ *
+*/ 
+
+Inputs.prototype.initEvents = function() {
+  // keys
+  window.addEventListener( 'keydown', onKeyEvent.bind(undefined,this,true), false )
+  window.addEventListener( 'keyup', onKeyEvent.bind(undefined,this,false), false )
+  // mouse buttons
+  this.element.addEventListener("mousedown", onMouseEvent.bind(undefined,this,true), false)
+  this.element.addEventListener("mouseup", onMouseEvent.bind(undefined,this,false), false)
+  this.element.oncontextmenu = onContextMenu.bind(undefined,this)
+  // mouse other
+  this.element.addEventListener("mousemove", onMouseMove.bind(undefined,this), false)
+}
+
+
+// Usage:  bind( bindingName, vkeyCode, vkeyCode.. )
+//    Note that inputs._bindings maps vkey codes to binding names
+//    e.g. this._bindings['a'] = 'move-left'
+Inputs.prototype.bind = function(binding) {
+  for (var i=1; i<arguments.length; ++i) {
+    var vkeyCode = arguments[i]
+    var arr = this._bindings[vkeyCode] || []
+    if (arr.indexOf(binding) == -1) {
+      arr.push(binding)
+    }
+    this._bindings[vkeyCode] = arr
+  }
+  this.state[binding] = !!this.state[binding]
+}
+
+// search out and remove all keycodes bound to a given binding
+Inputs.prototype.unbind = function(binding) {
+  for (var b in this._bindings) {
+    var arr = this._bindings[b]
+    var i = arr.indexOf(binding)
+    if (i>-1) { arr.splice(i,1) }
+  }
+}
+
+// tick function - clears out cumulative mouse movement state variables
+Inputs.prototype.tick = function() {
+  this.state.dx = this.state.dy = 0
+}
+
+
+
+Inputs.prototype.getBindings = function() {
+  var arr = []
+  for (var b in this._bindings) { arr.push(b) }
+  return arr
+}
+
+
+
+/*
+ *   INTERNALS - DOM EVENT HANDLERS
+*/ 
+
+
+function onKeyEvent(inputs, wasDown, ev) {
+  handleKeyEvent( ev.keyCode, vkey[ev.keyCode], wasDown, inputs, ev )
+}
+
+function onMouseEvent(inputs, wasDown, ev) {
+  // simulate a code out of range of vkey
+  var keycode = -1 - ev.button
+  var vkeycode = '<mouse '+ (ev.button+1) +'>' 
+  handleKeyEvent( keycode, vkeycode, wasDown, inputs, ev )
+  return false
+}
+
+function onContextMenu(inputs) {
+  // cancel context menu if there's a binding for right mousebutton
+  var arr = inputs._bindings['<mouse 3>']
+  if (arr) { return false }
+}
+
+function onMouseMove(inputs, ev) {
+  // for now, just populate the state object with mouse movement
+  var dx = ev.movementX || ev.mozMovementX || ev.webkitMovementX || 0,
+      dy = ev.movementY || ev.mozMovementY || ev.webkitMovementY || 0
+  inputs.state.dx += dx
+  inputs.state.dy += dy
+  // TODO: verify if this is working/useful during pointerlock?
+}
+
+
+/*
+ *   KEY BIND HANDLING
+*/ 
+
+
+function handleKeyEvent(keycode, vcode, wasDown, inputs, ev) {
+  var arr = inputs._bindings[vcode]
+  // don't prevent defaults if there's no binding
+  if (!arr) { return }
+  if (inputs.preventDefaults) ev.preventDefault()
+  if (inputs.stopPropagation) ev.stopPropagation()
+
+  // if the key's state has changed, handle an event for all bindings
+  var currstate = inputs._keyStates[keycode]
+  if ( XOR(currstate, wasDown) ) {
+    // for each binding: emit an event, and update cached state information
+    for (var i=0; i<arr.length; ++i) {
+      handleBindingEvent( arr[i], wasDown, inputs, ev )
+    }
+  }
+  inputs._keyStates[keycode] = wasDown
+}
+
+
+function handleBindingEvent(binding, wasDown, inputs, ev) {
+  // keep count of presses mapped by binding
+  // (to handle two keys with the same binding pressed at once)
+  var ct = inputs._bindPressCounts[binding] || 0
+  ct += wasDown ? 1 : -1
+  if (ct<0) { ct = 0 } // shouldn't happen
+  inputs._bindPressCounts[binding] = ct
+
+  // emit event if binding's state has changed
+  var currstate = inputs.state[binding]
+  if ( XOR(currstate, ct) ) {
+    var emitter = wasDown ? inputs.down : inputs.up
+    emitter.emit( binding, ev )
+  }
+  inputs.state[binding] = !!ct
+}
+
+
+/*
+ *    HELPERS
+ *
+*/
+
+
+// how is this not part of Javascript?
+function XOR(a,b) {
+  return a ? !b : b
+}
+
+
+
+
+
+},{"events":4,"vkey":3}],3:[function(require,module,exports){
+var ua = typeof window !== 'undefined' ? window.navigator.userAgent : ''
+  , isOSX = /OS X/.test(ua)
+  , isOpera = /Opera/.test(ua)
+  , maybeFirefox = !/like Gecko/.test(ua) && !isOpera
+
+var i, output = module.exports = {
+  0:  isOSX ? '<menu>' : '<UNK>'
+, 1:  '<mouse 1>'
+, 2:  '<mouse 2>'
+, 3:  '<break>'
+, 4:  '<mouse 3>'
+, 5:  '<mouse 4>'
+, 6:  '<mouse 5>'
+, 8:  '<backspace>'
+, 9:  '<tab>'
+, 12: '<clear>'
+, 13: '<enter>'
+, 16: '<shift>'
+, 17: '<control>'
+, 18: '<alt>'
+, 19: '<pause>'
+, 20: '<caps-lock>'
+, 21: '<ime-hangul>'
+, 23: '<ime-junja>'
+, 24: '<ime-final>'
+, 25: '<ime-kanji>'
+, 27: '<escape>'
+, 28: '<ime-convert>'
+, 29: '<ime-nonconvert>'
+, 30: '<ime-accept>'
+, 31: '<ime-mode-change>'
+, 27: '<escape>'
+, 32: '<space>'
+, 33: '<page-up>'
+, 34: '<page-down>'
+, 35: '<end>'
+, 36: '<home>'
+, 37: '<left>'
+, 38: '<up>'
+, 39: '<right>'
+, 40: '<down>'
+, 41: '<select>'
+, 42: '<print>'
+, 43: '<execute>'
+, 44: '<snapshot>'
+, 45: '<insert>'
+, 46: '<delete>'
+, 47: '<help>'
+, 91: '<meta>'  // meta-left -- no one handles left and right properly, so we coerce into one.
+, 92: '<meta>'  // meta-right
+, 93: isOSX ? '<meta>' : '<menu>'      // chrome,opera,safari all report this for meta-right (osx mbp).
+, 95: '<sleep>'
+, 106: '<num-*>'
+, 107: '<num-+>'
+, 108: '<num-enter>'
+, 109: '<num-->'
+, 110: '<num-.>'
+, 111: '<num-/>'
+, 144: '<num-lock>'
+, 145: '<scroll-lock>'
+, 160: '<shift-left>'
+, 161: '<shift-right>'
+, 162: '<control-left>'
+, 163: '<control-right>'
+, 164: '<alt-left>'
+, 165: '<alt-right>'
+, 166: '<browser-back>'
+, 167: '<browser-forward>'
+, 168: '<browser-refresh>'
+, 169: '<browser-stop>'
+, 170: '<browser-search>'
+, 171: '<browser-favorites>'
+, 172: '<browser-home>'
+
+  // ff/osx reports '<volume-mute>' for '-'
+, 173: isOSX && maybeFirefox ? '-' : '<volume-mute>'
+, 174: '<volume-down>'
+, 175: '<volume-up>'
+, 176: '<next-track>'
+, 177: '<prev-track>'
+, 178: '<stop>'
+, 179: '<play-pause>'
+, 180: '<launch-mail>'
+, 181: '<launch-media-select>'
+, 182: '<launch-app 1>'
+, 183: '<launch-app 2>'
+, 186: ';'
+, 187: '='
+, 188: ','
+, 189: '-'
+, 190: '.'
+, 191: '/'
+, 192: '`'
+, 219: '['
+, 220: '\\'
+, 221: ']'
+, 222: "'"
+, 223: '<meta>'
+, 224: '<meta>'       // firefox reports meta here.
+, 226: '<alt-gr>'
+, 229: '<ime-process>'
+, 231: isOpera ? '`' : '<unicode>'
+, 246: '<attention>'
+, 247: '<crsel>'
+, 248: '<exsel>'
+, 249: '<erase-eof>'
+, 250: '<play>'
+, 251: '<zoom>'
+, 252: '<no-name>'
+, 253: '<pa-1>'
+, 254: '<clear>'
+}
+
+for(i = 58; i < 65; ++i) {
+  output[i] = String.fromCharCode(i)
+}
+
+// 0-9
+for(i = 48; i < 58; ++i) {
+  output[i] = (i - 48)+''
+}
+
+// A-Z
+for(i = 65; i < 91; ++i) {
+  output[i] = String.fromCharCode(i)
+}
+
+// num0-9
+for(i = 96; i < 106; ++i) {
+  output[i] = '<num-'+(i - 96)+'>'
+}
+
+// F1-F24
+for(i = 112; i < 136; ++i) {
+  output[i] = 'F'+(i-111)
+}
+
+},{}],4:[function(require,module,exports){
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+function EventEmitter() {
+  this._events = this._events || {};
+  this._maxListeners = this._maxListeners || undefined;
+}
+module.exports = EventEmitter;
+
+// Backwards-compat with node 0.10.x
+EventEmitter.EventEmitter = EventEmitter;
+
+EventEmitter.prototype._events = undefined;
+EventEmitter.prototype._maxListeners = undefined;
+
+// By default EventEmitters will print a warning if more than 10 listeners are
+// added to it. This is a useful default which helps finding memory leaks.
+EventEmitter.defaultMaxListeners = 10;
+
+// Obviously not all Emitters should be limited to 10. This function allows
+// that to be increased. Set to zero for unlimited.
+EventEmitter.prototype.setMaxListeners = function(n) {
+  if (!isNumber(n) || n < 0 || isNaN(n))
+    throw TypeError('n must be a positive number');
+  this._maxListeners = n;
+  return this;
+};
+
+EventEmitter.prototype.emit = function(type) {
+  var er, handler, len, args, i, listeners;
+
+  if (!this._events)
+    this._events = {};
+
+  // If there is no 'error' event listener then throw.
+  if (type === 'error') {
+    if (!this._events.error ||
+        (isObject(this._events.error) && !this._events.error.length)) {
+      er = arguments[1];
+      if (er instanceof Error) {
+        throw er; // Unhandled 'error' event
+      }
+      throw TypeError('Uncaught, unspecified "error" event.');
+    }
+  }
+
+  handler = this._events[type];
+
+  if (isUndefined(handler))
+    return false;
+
+  if (isFunction(handler)) {
+    switch (arguments.length) {
+      // fast cases
+      case 1:
+        handler.call(this);
+        break;
+      case 2:
+        handler.call(this, arguments[1]);
+        break;
+      case 3:
+        handler.call(this, arguments[1], arguments[2]);
+        break;
+      // slower
+      default:
+        len = arguments.length;
+        args = new Array(len - 1);
+        for (i = 1; i < len; i++)
+          args[i - 1] = arguments[i];
+        handler.apply(this, args);
+    }
+  } else if (isObject(handler)) {
+    len = arguments.length;
+    args = new Array(len - 1);
+    for (i = 1; i < len; i++)
+      args[i - 1] = arguments[i];
+
+    listeners = handler.slice();
+    len = listeners.length;
+    for (i = 0; i < len; i++)
+      listeners[i].apply(this, args);
+  }
+
+  return true;
+};
+
+EventEmitter.prototype.addListener = function(type, listener) {
+  var m;
+
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  if (!this._events)
+    this._events = {};
+
+  // To avoid recursion in the case that type === "newListener"! Before
+  // adding it to the listeners, first emit "newListener".
+  if (this._events.newListener)
+    this.emit('newListener', type,
+              isFunction(listener.listener) ?
+              listener.listener : listener);
+
+  if (!this._events[type])
+    // Optimize the case of one listener. Don't need the extra array object.
+    this._events[type] = listener;
+  else if (isObject(this._events[type]))
+    // If we've already got an array, just append.
+    this._events[type].push(listener);
+  else
+    // Adding the second element, need to change to array.
+    this._events[type] = [this._events[type], listener];
+
+  // Check for listener leak
+  if (isObject(this._events[type]) && !this._events[type].warned) {
+    var m;
+    if (!isUndefined(this._maxListeners)) {
+      m = this._maxListeners;
+    } else {
+      m = EventEmitter.defaultMaxListeners;
+    }
+
+    if (m && m > 0 && this._events[type].length > m) {
+      this._events[type].warned = true;
+      console.error('(node) warning: possible EventEmitter memory ' +
+                    'leak detected. %d listeners added. ' +
+                    'Use emitter.setMaxListeners() to increase limit.',
+                    this._events[type].length);
+      if (typeof console.trace === 'function') {
+        // not supported in IE 10
+        console.trace();
+      }
+    }
+  }
+
+  return this;
+};
+
+EventEmitter.prototype.on = EventEmitter.prototype.addListener;
+
+EventEmitter.prototype.once = function(type, listener) {
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  var fired = false;
+
+  function g() {
+    this.removeListener(type, g);
+
+    if (!fired) {
+      fired = true;
+      listener.apply(this, arguments);
+    }
+  }
+
+  g.listener = listener;
+  this.on(type, g);
+
+  return this;
+};
+
+// emits a 'removeListener' event iff the listener was removed
+EventEmitter.prototype.removeListener = function(type, listener) {
+  var list, position, length, i;
+
+  if (!isFunction(listener))
+    throw TypeError('listener must be a function');
+
+  if (!this._events || !this._events[type])
+    return this;
+
+  list = this._events[type];
+  length = list.length;
+  position = -1;
+
+  if (list === listener ||
+      (isFunction(list.listener) && list.listener === listener)) {
+    delete this._events[type];
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
+
+  } else if (isObject(list)) {
+    for (i = length; i-- > 0;) {
+      if (list[i] === listener ||
+          (list[i].listener && list[i].listener === listener)) {
+        position = i;
+        break;
+      }
+    }
+
+    if (position < 0)
+      return this;
+
+    if (list.length === 1) {
+      list.length = 0;
+      delete this._events[type];
+    } else {
+      list.splice(position, 1);
+    }
+
+    if (this._events.removeListener)
+      this.emit('removeListener', type, listener);
+  }
+
+  return this;
+};
+
+EventEmitter.prototype.removeAllListeners = function(type) {
+  var key, listeners;
+
+  if (!this._events)
+    return this;
+
+  // not listening for removeListener, no need to emit
+  if (!this._events.removeListener) {
+    if (arguments.length === 0)
+      this._events = {};
+    else if (this._events[type])
+      delete this._events[type];
+    return this;
+  }
+
+  // emit removeListener for all listeners on all events
+  if (arguments.length === 0) {
+    for (key in this._events) {
+      if (key === 'removeListener') continue;
+      this.removeAllListeners(key);
+    }
+    this.removeAllListeners('removeListener');
+    this._events = {};
+    return this;
+  }
+
+  listeners = this._events[type];
+
+  if (isFunction(listeners)) {
+    this.removeListener(type, listeners);
+  } else {
+    // LIFO order
+    while (listeners.length)
+      this.removeListener(type, listeners[listeners.length - 1]);
+  }
+  delete this._events[type];
+
+  return this;
+};
+
+EventEmitter.prototype.listeners = function(type) {
+  var ret;
+  if (!this._events || !this._events[type])
+    ret = [];
+  else if (isFunction(this._events[type]))
+    ret = [this._events[type]];
+  else
+    ret = this._events[type].slice();
+  return ret;
+};
+
+EventEmitter.listenerCount = function(emitter, type) {
+  var ret;
+  if (!emitter._events || !emitter._events[type])
+    ret = 0;
+  else if (isFunction(emitter._events[type]))
+    ret = 1;
+  else
+    ret = emitter._events[type].length;
+  return ret;
+};
+
+function isFunction(arg) {
+  return typeof arg === 'function';
+}
+
+function isNumber(arg) {
+  return typeof arg === 'number';
+}
+
+function isObject(arg) {
+  return typeof arg === 'object' && arg !== null;
+}
+
+function isUndefined(arg) {
+  return arg === void 0;
+}
+
+},{}]},{},[1]);
